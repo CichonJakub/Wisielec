9be3d25b7a6eaeb37fece3c65154e5b30814448b
Jakub Cichoń <48649767+CichonJakub@users.noreply.github.com>
2020-07-08 19:31:18 +0200

    client_multicast

--- client.py
+++ client.py
@@ -1,26 +1,36 @@
 import sys
 import socket
-
-
-# argument 0 to jest nazawa pliku jaki odpalamy, uwaga: dane zaczynaja sie od argv[1]
-if len(sys.argv) == 3:
-    serverAddressPort = (str(sys.argv[1]), int(sys.argv[2]))
-else:
-    # MCAST_GRP = '224.1.1.1'
-    # MCAST_PORT = 5007
-    #
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    # sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-    # sock.sendto('Hello World!', (MCAST_GRP, MCAST_PORT))
-    serverAddressPort   = ("127.0.0.1", 8080)
+import struct
 
 msgFromClient       = "Hello slave!"
 bytesToSend         = str.encode(msgFromClient)
 #serverAddressPort   = ("127.0.0.1", 8080)
 bufferSize          = 1024
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+# argument 0 to jest nazawa pliku jaki odpalamy, uwaga: dane zaczynaja sie od argv[1]
+if len(sys.argv) == 3:
+    serverAddressPort = (str(sys.argv[1]), int(sys.argv[2]))
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+else:
+    MCAST_GRP = '224.1.1.5'
+    MCAST_PORT = 5008
+
+    UDPClientSocket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_IP)
+    UDPClientSocket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    UDPClientSocket2.bind('', MCAST_PORT)
+
+    mreq = struct.pack('4sl', socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+    UDPClientSocket2.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    UDPClientSocket2.sendto(bytesToSend, (MCAST_GRP, MCAST_PORT))
+
+    #creating normal socket to have responeses on unicast
+    serverAddressPort = ("127.0.0.1", 8080)
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+
+
 
 for i in range(5):
         msgFromServer = UDPClientSocket.recvfrom(bufferSize)
